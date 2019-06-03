1bd3eb29e49ce388158ff3fe8e5b17ff3e842505
simon <gatheru.simon432@gmail.com>
2019-06-03 12:45:38 +0300

    intro url, view defined and app url linked to project

--- app/models.py
+++ app/models.py
@@ -4,7 +4,7 @@ class Profile(models.Model):
     # profile_pic = models.ImageField()
     username = models.CharField(max_length=20)
     email = models.EmailField()
-    mobile_no = models.IntegerField(max_length=15)
+    mobile_no = models.CharField(max_length=15)
 
     # Relationships and Foreign Keys
 
--- app/views.py
+++ app/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+def intro(request):
+    return render(request,'intro.html')
