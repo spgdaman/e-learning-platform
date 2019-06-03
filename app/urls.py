a38378d5f881d1172e017f8cc067494622d67da5
simon <gatheru.simon432@gmail.com>
2019-06-03 14:07:14 +0300

    navbar added to index page

--- app/urls.py
+++ app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.intro, name='intro'),
+    url(r'^index/$', views.index, name='index')
 ]
--- app/views.py
+++ app/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def intro(request):
     return render(request,'intro.html')
+
+def index(request):
+    return render(request,'index.html')
