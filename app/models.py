aab829a1e328475a38434f4ea885d38d77129e45
simon <gatheru.simon432@gmail.com>
2019-06-03 12:23:09 +0300

    course model created

--- app/models.py
+++ app/models.py
@@ -7,4 +7,8 @@ class Profile(models.Model):
     mobile_no = models.IntegerField(max_length=15)
 
     # Relationships and Foreign Keys
-    
+
+class Course(models.Model):
+    course_name = models.CharField(max_length=40)
+    course_description = models.CharField(max_length=150)
+
