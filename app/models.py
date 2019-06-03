fd1030b8e90e67036750b7d71abf36c903497683
simon <gatheru.simon432@gmail.com>
2019-06-03 12:20:28 +0300

    profile model created

--- app/models.py
+++ app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Profile(models.Model):
+    # profile_pic = models.ImageField()
+    username = models.CharField(max_length=20)
+    email = models.EmailField()
+    mobile_no = models.IntegerField(max_length=15)
+
+    # Relationships and Foreign Keys
+    
