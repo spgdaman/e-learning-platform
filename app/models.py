3b5e6bff43662db4b8cbff56a0fc5a45fb1b905c
simon <gatheru.simon432@gmail.com>
2019-06-04 16:15:19 +0300

    profile picture field added

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Profile(models.Model):
-    # profile_pic = models.ImageField()
+    profile_pic = models.ImageField()
     username = models.CharField(max_length=20)
     email = models.EmailField()
     mobile_no = models.CharField(max_length=15)
