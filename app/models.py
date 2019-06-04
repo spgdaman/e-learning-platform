a5c433681b3b777d2805f2d92b05fd7c33712bbb
simon <gatheru.simon432@gmail.com>
2019-06-04 17:18:02 +0300

    media route added

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Profile(models.Model):
-    profile_pic = models.ImageField(upload_to='profile_pics')
+    profile_pic = models.ImageField(upload_to='profile_pics/')
     username = models.CharField(max_length=20)
     email = models.EmailField()
     mobile_no = models.CharField(max_length=15)
--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^$', views.intro, name='intro'),
@@ -7,3 +9,6 @@ urlpatterns = [
     url(r'^profile/update/$', views.update_profile, name='update_profile'),
     url(r'^profile/$', views.profile, name='profile'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
