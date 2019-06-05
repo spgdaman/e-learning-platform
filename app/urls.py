33b68b9a7954c6672e4e200efcdb42a789212a9a
simon <gatheru.simon432@gmail.com>
2019-06-05 14:18:22 +0300

    assignment submission form added and tested

--- app/urls.py
+++ app/urls.py
@@ -7,9 +7,9 @@ urlpatterns = [
     url(r'^$', views.intro, name='intro'),
     url(r'^index/$', views.index, name='index'),
     url(r'^profile/update/$', views.update_profile, name='update_profile'),
-    url(r'^profile/$', views.profile, name='profile'),
+    url(r'^profile/(\d+)', views.profile, name='profile'),
     url(r'^enroll/course/$', views.enroll_course, name='enroll_course'),
-    url(r'submit/assignment/$', views.submit_assignment, name="submit_assignment"),
+    url(r'^submit/assignment/$', views.submit_assignment, name="submit_assignment"),
 ]
 
 if settings.DEBUG:
--- app/views.py
+++ app/views.py
@@ -21,8 +21,8 @@ def update_profile(request):
         form = ProfileForm()
     return render(request,'profileupdate.html',{'form':form})
 
-def profile(request):
-    profile = Profile.objects.all()
+def profile(request,profile_id):
+    profile = Profile.objects.filter(id=profile_id)
     return render(request,'profile.html',{"profile":profile})
 
 def enroll_course(request):
@@ -39,4 +39,14 @@ def enroll_course(request):
     return render(request,'enrollcourse.html',{'form':form})
 
 def submit_assignment(request):
-    pass
+    current_user = request.user
+    if request.method == 'POST':
+        form = AssignmentForm(request.POST, request.FILES)
+        if form.is_valid():
+            assignment = form.save(commit=False)
+            assignment.profile = current_user
+            assignment.save()
+            return redirect(index)
+    else:
+        form = AssignmentForm()
+    return render(request,'submitassignment.html',{"form":form})
