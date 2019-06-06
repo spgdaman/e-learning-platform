c1c285aabdb510e1c1c08eb71c6eb1a2a24d56e3
simon <gatheru.simon432@gmail.com>
2019-06-06 10:25:06 +0300

    navbar refactored to diplay profile details of current user

--- app/views.py
+++ app/views.py
@@ -55,16 +55,16 @@ def submit_assignment(request):
         form = AssignmentForm()
     return render(request,'submitassignment.html',{"form":form})
 
-# def signup(request):
-#     if request.method == 'POST':
-#         form = UserCreationForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             username = form.cleaned_data.get('username')
-#             raw_password = form.cleaned_data.get('password1')
-#             user = authenticate(username=username, password=raw_password)
-#             login(request, user)
-#             return redirect('index')
-#     else:
-#         form = UserCreationForm()
-#     return render(request, 'signup.html', {'form': form})
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
